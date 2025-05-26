1d5da1e0aa36d7bc135dab9b55c50084372143b5
MaickolHVza <maic.hernandez.dev@gmail.com>
2025-05-26 11:30:09 -0400

    agregar comentarios
    comentarios de json y enrutamiento

--- src/main/java/com/fitconnect/agendar/AgendarApplication.java
+++ src/main/java/com/fitconnect/agendar/AgendarApplication.java
@@ -11,3 +11,23 @@ public class AgendarApplication {
 	}
 
 }
+//http://localhost:8080/api/agendas
+//{
+//  "nombreCliente": "Benjamin Tapion",
+//  "rutCliente": "21345678-9",
+//  "idServicio": 2,
+//  "fecha": "2025-05-19",
+//  "hora": "12:30",
+//  "emailCliente": "benjamin.munoz@mail.com"
+//}
+
+//{
+//  "nombreCliente": "agustin fernandez",
+//  "rutCliente": "23345678-9",
+//  "idServicio": 2,
+//  "fecha": "2025-05-26",
+//  "hora": "12:50",
+//  "emailCliente": "agustin.fer@mail.com"
+//}
+
+
